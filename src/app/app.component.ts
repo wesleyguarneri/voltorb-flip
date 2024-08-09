65a5cdfa94186b5b1714ad80ade4ecda5abc6873
wlg223 <wesguarneri@gmail.com>
2024-08-08 20:52:28 -0400

    updating global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BoardComponent } from './components/board/board.component';
+import { GameScreenComponent } from './components/game-screen/game-screen.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,BoardComponent],
+  imports: [RouterOutlet,GameScreenComponent,MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
