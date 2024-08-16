9025c50f5b90a72fa647657b7dc0036e14777be0
wlg223 <wesguarneri@gmail.com>
2024-08-15 23:39:11 -0400

    adding mobile styling

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-EZMJ3KQ5.mjs";import"./chunk-U6Y5SREQ.mjs";import"./chunk-DAJ6VHT2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-5H4RN27K.mjs";import"./chunk-U6Y5SREQ.mjs";import"./chunk-DAJ6VHT2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
