2b804299c4c1fc9dab5abc12b7fa17d110d9704d
wlg223 <wesguarneri@gmail.com>
2024-09-17 21:27:54 -0400

    adding mobile animations for memo button

--- src/app/components/memo-button/memo-button.component.ts
+++ src/app/components/memo-button/memo-button.component.ts
@@ -1,6 +1,6 @@
 import { trigger, transition, style, animate, state } from '@angular/animations';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { BoardStateService } from '../../services/board-state.service';
@@ -27,18 +27,35 @@ import { BoardStateService } from '../../services/board-state.service';
         style({transform: 'translateY(-100%)'}),
         animate('200ms ease-in', style({transform: 'translateY(0%)',opacity: 1}))
       ]),
+      state('open-mobile', style({
+        opacity: 1
+      })),
+      state('closed-mobile', style({
+        opacity: 0
+      })),
+      transition('open-mobile => closed-mobile', [
+        style({transform: 'translateX(0%)'}),
+        animate('200ms ease-in', style({transform: 'translateX(-100%)',opacity: 0}))
+      ]),
+      transition('closed-mobile => open-mobile', [
+        style({transform: 'translateX(-100%)'}),
+        animate('200ms ease-in', style({transform: 'translateX(0%)',opacity: 1}))
+      ]),
     ]),
   ],
 })
 export class MemoButtonComponent {
-
   isOpen: boolean = false;
+  isMobile: boolean = false;
 
   constructor(
     private boardState: BoardStateService
   ){}
 
-  ngOnInit(){}
+  ngOnInit(){
+    this.checkScreenWidth();
+    console.log(this.isMobile)
+  }
   
   toggleMemoButton(){
     this.isOpen = !this.isOpen
@@ -46,5 +63,14 @@ export class MemoButtonComponent {
 
   setBoardState(state: string){
     this.boardState.setState(state)
+  }
+
+  checkScreenWidth() {
+    this.isMobile = window.innerWidth < 400;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isMobile = event.target.innerWidth < 400;
   }
 }
